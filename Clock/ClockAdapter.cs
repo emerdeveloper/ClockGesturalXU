e5132238fee4d87070487eb452b7a0f291b3622f
Emerson Javid Gonzalez Morales <1388310@SOAM.TCS.com>
2017-08-29 10:57:58 -0500

    Exercise 3: Implement gestural navigation with ViewPager. Here you will code gestural navigation to let the user swipe to switch between views.

---
+++ Clock/ClockAdapter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Clock
+{
+    public class ClockAdapter : FragmentPagerAdapter
+    {
+        //field to store a collection of fragments 
+        Android.Support.V4.App.Fragment[] fragments;
+
+        //constructor needs to chain to this base constructor and pass the manager
+        public ClockAdapter(
+            Android.Support.V4.App.FragmentManager fragmentManager, 
+            Android.Support.V4.App.Fragment[] fragments) : base(fragmentManager)
+        {
+            this.fragments = fragments;
+        }
+
+        //Number of Fragments in the collection
+        public override int Count
+        {
+            get { return fragments.Length; }
+        }
+
+        public override Android.Support.V4.App.Fragment GetItem(int position)//It receives an integer which identifies the fragment that is being requested by the ViewPager.
+        {
+            //Use that integer as an index into your fragment collection to return the appropriate one.
+            return fragments[position];
+        }
+    }
+}
--- Clock/MainActivity.cs
+++ Clock/MainActivity.cs
@@ -10,6 +10,21 @@ namespace Clock
 		{
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.Main);
-		}
-	}
+
+            //array of fragments and populate it with an object of each of the three clock fragments
+            var fragments = new Android.Support.V4.App.Fragment[] {
+                new TimeFragment(),
+                new StopwatchFragment(),
+                new AboutFragment(),
+            };
+
+            var clockAdapter = new ClockAdapter(
+                base.SupportFragmentManager, //value from the SupportFragmentManager property you inherit from the FragmentActivity base class
+                fragments);
+            //Reference to ViewPager where will show the fragments
+            var viewPager = FindViewById<Android.Support.V4.View.ViewPager>(Resource.Id.viewPager);
+            //set ours Adapter
+            viewPager.Adapter = clockAdapter;
+        }
+    }
 }
